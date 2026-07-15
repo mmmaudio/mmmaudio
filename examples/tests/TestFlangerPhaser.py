1284a0e4fdeb97979cdceeb010915bc3aecd0b8d
spluta <spluta@gmail.com>
2026-07-15 11:01:36 -0400

    fix flanger test file

--- examples/tests/TestFlangerPhaser.py
+++ examples/tests/TestFlangerPhaser.py
@@ -1,9 +1,9 @@
 from mmm_python import *
-m_a = MMMAudio(128, graph_name="TestFlangerPhaser", package_name="user_files")
+m_a = MMMAudio(128, graph_name="TestFlangerPhaser", package_name="examples.tests")
 m_a.start_audio() 
 
 m_a.send_int("which_source", 1) # 0 for sample, 1 for noise
-m_a.send_float("which_fx", 1) # 0 for phaser, 1 for flanger
+m_a.send_float("which_fx", 0) # 0 for phaser, 1 for flanger
 m_a.send_float("center_freq", 500.)
 m_a.send_float("Q", 0.5) # phaser only
 m_a.send_float("feedback_coef", 0.7) # flanger only
